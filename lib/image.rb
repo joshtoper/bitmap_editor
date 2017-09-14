184c07f06384e960fc39ab859967465b63d4e55b
Josh <joshuatoper@gmail.com>
2017-09-14 16:50:56 +0100

    Remove some whitespace.

--- lib/image.rb
+++ lib/image.rb
@@ -2,9 +2,9 @@ module BitmapEditor
   class Image
     class InvalidWidthError < StandardError; end
     class InvalidHeightError < StandardError; end
-    
+
     attr_reader :width, :height
-    
+
     ACCEPTED_WIDTH_RANGE = (1..250).freeze
     ACCEPTED_HEIGHT_RANGE = (1..250).freeze
 
