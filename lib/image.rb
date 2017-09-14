c74624be90a94ac4274954b60cb989104ddd6e40
Josh <joshuatoper@gmail.com>
2017-09-14 16:47:49 +0100

    Implement error handling for Create class and Image class.

--- lib/image.rb
+++ lib/image.rb
@@ -1,10 +1,18 @@
 module BitmapEditor
   class Image
+    class InvalidWidthError < StandardError; end
+    class InvalidHeightError < StandardError; end
+    
     attr_reader :width, :height
+    
+    ACCEPTED_WIDTH_RANGE = (1..250).freeze
+    ACCEPTED_HEIGHT_RANGE = (1..250).freeze
 
     def initialize(width, height)
       @width = width
       @height = height
+      raise InvalidWidthError, 'Invalid width specified' unless ACCEPTED_WIDTH_RANGE.include?(width.to_i)
+      raise InvalidHeightError, 'Invalid height specified' unless ACCEPTED_HEIGHT_RANGE.include?(height.to_i)
     end
   end
 end
--- spec/bitmap_editor/command/create_spec.rb
+++ spec/bitmap_editor/command/create_spec.rb
@@ -11,6 +11,32 @@ module BitmapEditor
           expect(cmd.image.width).to eq 5
         end
       end
+
+      context 'with invalid params' do
+        it 'raises an exception when the specified width is below the allowed minimum' do
+          cmd = described_class.new(Image::ACCEPTED_WIDTH_RANGE.first - 1, 10)
+          expect { cmd.run }
+            .to raise_error BitmapEditor::Image::InvalidWidthError
+        end
+
+        it 'raises an exception when the specified width is above the allowed minimum' do
+          cmd = described_class.new(Image::ACCEPTED_WIDTH_RANGE.last + 1, 10)
+          expect { cmd.run }
+            .to raise_error BitmapEditor::Image::InvalidWidthError
+        end
+
+        it 'raises an exception when the specified height is below the allowed range' do
+          cmd = described_class.new(10, Image::ACCEPTED_HEIGHT_RANGE.first - 1)
+          expect { cmd.run }
+            .to raise_error BitmapEditor::Image::InvalidHeightError
+        end
+
+        it 'raises an exception when the specified height is above the allowed range' do
+          cmd = described_class.new(10, Image::ACCEPTED_HEIGHT_RANGE.last + 1)
+          expect { cmd.run }
+            .to raise_error BitmapEditor::Image::InvalidHeightError
+        end
+      end
     end
   end
 end
