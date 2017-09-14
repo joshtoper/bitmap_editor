fe7843ca1fb11c419550c0f0c908363419c9b805
Josh <joshuatoper@gmail.com>
2017-09-14 16:50:12 +0100

    Tidy up the basic error handling in the Image class.

--- lib/image.rb
+++ lib/image.rb
@@ -9,8 +9,14 @@ module BitmapEditor
     ACCEPTED_HEIGHT_RANGE = (1..250).freeze
 
     def initialize(width, height)
+      validate_dimensions(width, height)
       @width = width
       @height = height
+    end
+
+    private
+
+    def validate_dimensions(width, height)
       raise InvalidWidthError, 'Invalid width specified' unless ACCEPTED_WIDTH_RANGE.include?(width.to_i)
       raise InvalidHeightError, 'Invalid height specified' unless ACCEPTED_HEIGHT_RANGE.include?(height.to_i)
     end
